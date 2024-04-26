3888a7f02f39acbefbb7f05f6e3f73744752264b
mansapatel111 <mansapatel111@gmail.com>
2024-04-26 14:27:19 -0400

    # Cell and Board are updated

--- sudoku.py
+++ sudoku.py
@@ -1,6 +1,8 @@
 import pygame
 import sys
 from board import *
+from cell import *
+from sudoku_generator import *
 
 # Initialize Pygame
 pygame.init()
@@ -15,38 +17,23 @@ pygame.display.set_caption("SUDOKU")
 
 def main():
     sudoku_board = Board(width, height, screen, difficulty="medium")
+    # sudoku_board = generate_sudoku(9, 45)  # Change the parameters as needed
+    # for row in range(9):
+    #     for col in range(9):
+    #         value = sudoku_board[row][col]
+    #         if value != 0:
+    #             board.cells[row][col].set_sketched_value(value)
 
     # Main game loop
     running = True
     while running:
+
+        # for event in pygame.event.get():
+        #     if event.type == pygame.QUIT:
+        #         running = False
         sudoku_board.handle_events()
         sudoku_board.draw()
         pygame.display.flip()
-        # for event in pygame.event.get():
-        #     if event.type == pygame.QUIT:
-        #         pygame.quit()
-        #         sys.exit()
-        #     elif event.type == pygame.MOUSEBUTTONDOWN:
-        #         # Get the position of the mouse click
-        #         x, y = pygame.mouse.get_pos()
-        #         # Check if a cell was clicked
-        #         clicked_cell = sudoku_board.click(x, y)
-        #         if clicked_cell:
-        #             # Update the selected cell
-        #             sudoku_board.select(*clicked_cell)  # Unpack the tuple
-        #             print("Clicked cell:", clicked_cell)  # For debugging purposes
-        #     elif event.type == pygame.KEYDOWN:
-        #         # Handle keyboard input
-        #         if pygame.K_1 <= event.key <= pygame.K_9:  # Check if the pressed key is a number key
-        #             value = event.key - pygame.K_0  # Convert key code to integer value
-        #             # Place the value in the selected cell
-        #             sudoku_board.place_number(value)
-        #         elif event.key == pygame.K_DELETE:  # Check if the pressed key is the delete key
-        #             sudoku_board.clear(clicked_cell)
-
-        # Update the display
-        # sudoku_board.draw()
-        # pygame.display.flip()
 
 if __name__ == "__main__":
     main()
