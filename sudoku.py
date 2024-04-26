29eede32bbf2a1f85cc7bb5a598cdbe3157cc7b0
HussamSaadi <tk2husam@gmail.com>
2024-04-26 17:54:39 -0400

    updated

--- sudoku.py
+++ sudoku.py
@@ -8,7 +8,7 @@ from sudoku_generator import *
 pygame.init()
 
 # Set up the display
-width, height = 750, 750
+width, height = 800, 800
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("SUDOKU")
 
@@ -18,16 +18,14 @@ pygame.display.set_caption("SUDOKU")
 def main():
     sudoku_board = Board(width, height, screen, difficulty="medium")
 
-    sudoku_generator = SudokuGenerator(9, 45)
-    sudoku_generator.fill_values()
-    sudoku_generator.remove_cells()
+    # Generate initial board
+    generated_board = generate_sudoku(9, 45)
 
-    generated_board = sudoku_generator.get_board()
     for row in range(9):
         for col in range(9):
             value = generated_board[row][col]
             if value != 0:
-                sudoku_board.cells[row][col].set_sketched_value(value)
+                sudoku_board.cells[row][col].set_cell_value(value)
 
     running = True
     while running:
@@ -39,3 +37,59 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import pygame
+# import sys
+# from board import *
+# from cell import *
+# from sudoku_generator import *
+#
+# # Initialize Pygame
+# pygame.init()
+#
+# # Set up the display
+# width, height = 750, 750
+# screen = pygame.display.set_mode((width, height))
+# pygame.display.set_caption("SUDOKU")
+#
+# # Define colors
+#
+#
+# def main():
+#     sudoku_board = Board(width, height, screen, difficulty="medium")
+#
+#     sudoku_generator = SudokuGenerator(9, 45)
+#     sudoku_generator.fill_values()
+#     sudoku_generator.remove_cells()
+#
+#     generated_board = sudoku_generator.get_board()
+#     for row in range(9):
+#         for col in range(9):
+#             value = generated_board[row][col]
+#             if value != 0:
+#                 sudoku_board.cells[row][col].set_sketched_value(value)
+#
+#     running = True
+#     while running:
+#         sudoku_board.draw()
+#         sudoku_board.handle_events()
+#         if sudoku_board.is_full():
+#             print("Congratulations! You have solved the Sudoku!")
+#             running = False
+#
+# if __name__ == "__main__":
+#     main()
