92eb9ac2906db6abb0b5d369d26aee630d00b156
mansapatel111 <mansapatel111@gmail.com>
2024-04-26 17:51:18 -0400

    # adding three new buttons

--- sudoku.py
+++ sudoku.py
@@ -1,26 +1,34 @@
 import pygame
-import sys
-from board import *
-from cell import *
+from board import Board
 from sudoku_generator import *
 
 # Initialize Pygame
 pygame.init()
 
 # Set up the display
-width, height = 750, 750
+width, height = 800, 800
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("SUDOKU")
 
 # Define colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+PINK = (255, 174, 201, 255)
+BLUE = (0, 162, 232, 255)
+LIGHT_BLUE = (173, 216, 230)
 
 
 def main():
     sudoku_board = Board(width, height, screen, difficulty="medium")
 
+    print("Sudoku board created")
+
     sudoku_generator = SudokuGenerator(9, 45)
+    print("1")
     sudoku_generator.fill_values()
+    print("2s")
     sudoku_generator.remove_cells()
+    print("Sudoku board generated")
 
     generated_board = sudoku_generator.get_board()
     for row in range(9):
@@ -33,9 +41,12 @@ def main():
     while running:
         sudoku_board.draw()
         sudoku_board.handle_events()
+        pygame.display.flip()
         if sudoku_board.is_full():
             print("Congratulations! You have solved the Sudoku!")
             running = False
+    print("Game loop exited")
 
 if __name__ == "__main__":
     main()
+
