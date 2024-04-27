b62fb6a3ad4149d53c72cf158bd34fcf579af81c
HussamSaadi <tk2husam@gmail.com>
2024-04-26 23:06:09 -0400

    Working version, kindly accept this version. Have a wonderful day!

--- sudoku.py
+++ sudoku.py
@@ -10,16 +10,23 @@ pygame.init()
 # Set up the display
 width, height = 800, 800
 screen = pygame.display.set_mode((width, height))
+# font = pygame.font.SysFont("arialblack", 40)
 pygame.display.set_caption("SUDOKU")
 
 # Define colors
-
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+PINK = (255, 174, 201, 255)
+BLUE = (0, 162, 232, 255)
+LIGHT_BLUE = (173, 216, 230)
 
 def main():
+
+    easy, medium, hard = "easy", "medium", "hard"
     sudoku_board = Board(width, height, screen, difficulty="medium")
 
     # Generate initial board
-    generated_board = generate_sudoku(9, 1)
+    generated_board = generate_sudoku(9, 40)
 
     for row in range(9):
         for col in range(9):
@@ -31,9 +38,9 @@ def main():
     while running:
         sudoku_board.draw()
         sudoku_board.handle_events()
-        if sudoku_board.is_full():
-            print("Congratulations! You have solved the Sudoku!")
-            running = False
+        # if sudoku_board.is_full():
+        #     print("Congratulations! You have solved the Sudoku!")
+        #     running = False
 
 if __name__ == "__main__":
     main()
